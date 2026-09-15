3af056aa0e83e2b4aea52cebe673c8cca7c4ea59
luisaaan <125052371@upq.edu.mx>
2026-09-14 20:28:28 -0600

    Practica 1

---
+++ introJS/holaNode.js
@@ -0,0 +1,14 @@
+
+console.log("Hola mundo NODE");
+
+let edad1 = 20;
+let edad2 =10;
+
+console.log("Edad promedio: ");
+console.log((edad1 + edad2) / 2);
+
+console.time("miProceso");
+    for(i = 0; i < 100000000; i++){
+    
+    }
+console.timeEnd("miProceso");
