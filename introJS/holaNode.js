746507899d9af247f396286831cc1254679e360c
luisaaan <125052371@upq.edu.mx>
2026-09-18 18:49:38 -0600

    Modificación del código

--- introJS/holaNode.js
+++ introJS/holaNode.js
@@ -8,7 +8,5 @@ console.log("Edad promedio: ");
 console.log((edad1 + edad2) / 2);
 
 console.time("miProceso");
-    for(i = 0; i < 100000000; i++){
-    
-    }
+    for(i = 0; i < 100000000; i++){}
 console.timeEnd("miProceso");
